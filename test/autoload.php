ae373a9149449278f8c4bb37cdbdc54a198c2918
Dennis Birkholz <dennis@birkholz.biz>
2014-09-08 03:15:20 +0200

    Fix autoloader

--- test/autoload.php
+++ test/autoload.php
@@ -1,9 +1,19 @@
 <?php
 
-if (is_dir(__DIR__ . '/../vendor')) {
-	$autoloader = require_once(__DIR__ . '/../vendor/autoload.php');
-} else {
-	$autoloader = require_once(__DIR__ . '/../../../vendor/autoload.php');
+$autoloader_locations = [
+	__DIR__ . '/../vendor/autoload.php',          // Package is used as standalone, so vendor dir is installed below package root
+	__DIR__ . '/../../../../vendor/autoload.php', // Package is installed as a dependency with composer and resides inside vendor dir
+];
+
+foreach ($autoloader_locations AS $autoloader_location) {
+	if (\file_exists($autoloader_location)) {
+		$autoloader = require_once($autoloader_location);
+		break;
+	}
 }
 
-$autoloader->addPsr4('nexxes\\tokenizer\\', __DIR__);
+if (!isset($autoloader)) {
+	throw new \RuntimeException('Can not find composer autoloader. Run "composer update" to initialize dependencies and make sure you did not change the vendor directory name.');
+}
+
+$autoloader->addPsr4('nexxes\\tokenmatcher\\', __DIR__);
